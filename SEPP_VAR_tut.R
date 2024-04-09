2aef368d97680dc17f17a80f370e969563d7f0db
chrishuong95@gmail.com <chrishuong95@gmail.com>
2024-04-09 10:34:52 -0500

    asdas

---
+++ SEPP_VAR_tut.R
@@ -0,0 +1,140 @@
+#################################################
+##                                             ##
+## Supplementary code for manuscript titled:   ##
+##                                             ##
+## Network Approaches for Physical Activity    ##
+## and Mental Health Research                  ##
+##                                             ## 
+##                                             ##  
+## Christopher Huong: chrishuong95@gmail.com   ##
+## Emily E. Bernstein                          ##
+## Joshua E. Curtiss                           ##
+## Denver M. Brown                             ##
+##                                             ##
+## 3/26/2024                                   ##
+##                                             ##
+#################################################
+
+
+library(MASS) 
+library(graphicalVAR)
+library(qgraph)
+library(corpcor)
+library(ggplot2)
+library(dplyr)
+
+
+# Specify a data generating model
+# Simulate an individuals data
+# then fit the an unregularized and regularized graphical VAR model
+nVar <- 4 
+nTime <- 200 
+varnames <- c("Depr", "Stress", "Fatigue", "PA")
+
+# Specify a matrix of hypothetical lagged relations
+# Rows = node of origin, columns = destination
+# Diagonals = autocorrelations
+Phi <- matrix(c(0.3, 0.0, 0.0, 0.0,
+                0.6, 0.0, 0.0, 0.0,
+                0.6, 0.3, 0.3,-0.6,
+               -0.6, 0.0,-0.6, 0.0), nrow=4,byrow=T)
+
+truePDC <- qgraph(Phi, layout="circle",
+                  theme="colorblind", labels=varnames)
+
+
+eigen(Phi)$values
+
+# Specify a contemporaneous variance-covariance matrix 
+Sigma <- matrix(c(1.0,  0.1,  0.5, -0.1,
+                  0.1,  1.0,  0.2,  0.3,
+                  0.5,  0.2,  1.0, -0.2,
+                 -0.1,  0.3, -0.2,  1.0), nrow=4,byrow=T)
+
+# True partial contemporaneous correlations (PCC)
+# Sigma is inverted (to obtain the precision matrix) and standardized 
+# to produce the partial correlation matrix
+truePCC <- qgraph(Sigma, graph="pcor",
+                  layout="circle", theme="colorblind", labels=varnames)
+
+# Check if positive-definite (eigenvalues > 0)
+eigen(Sigma)$values > 0
+
+# Simulate innovations (dynamic errors) from multivariate Gaussian distribution with covariance matrix Sigma
+set.seed(999)
+U <- mvrnorm(n = nTime, mu = rep(0, nVar), Sigma = Sigma) 
+
+p1 <- matrix(0, nrow=nTime, ncol=nVar) 
+
+# Produce data set with 1-lagged relationships according to Phi
+p1[1,] <- U[1,] 
+for (t in 2: nTime){ 
+  p1[t,] <- p1[t-1,] %*% Phi + U[t,] 
+} 
+
+colnames(p1) <- varnames
+# Could add more noise to all observations from rnorm()
+
+# Standardize to make coefficients comparable (this is done in graphicalVAR by default)
+p1_std <- apply(p1, 2, scale)
+
+# Fit unregularized VAR
+# Initialize null matrices
+unreg_PDC <- matrix(0, nVar, nVar)
+unreg_resid <- matrix(0, nrow(p1_std)-1, nVar)
+# GLM loop to predict each var at time=t-1, from itself and every other var at t
+for(i in 1:nVar){
+  res <- glm(p1_std[-1,varnames[i]] ~ p1_std[-nrow(p1_std),varnames],
+             family="gaussian")
+  unreg_PDC[, i] <- res$coefficients[-1]
+  unreg_resid[, i] <- res$residuals
+}
+
+# Since data were standardized, coefficients represent partial correlations
+unreg_PDC |> qgraph(layout="circle", theme="colorblind", labels=varnames)
+unreg_PCC <- cov(unreg_resid) |> cor2pcor() |> qgraph(layout="circle", theme="colorblind", labels=varnames)
+
+# True edges are obtained, but there are also many spurious edges. 
+# We want to obtain a sparse network where edges that are likely to be spurious are shrunk to 0
+# Thus we apply lasso regularization, with tuning parameters (lambda) controlling sparsity in Beta (matrix encoding lagged effects) and Kappa (matrix encoding contemporaneous effects)
+# Tuning parameters are selected using a grid search by minimizing the Extended Bayesian Information Criterion
+# The EBIC hyperparameter (gamma) is specified to prefer discovery (gamma=0) or parsimony (gamma=0.5) 
+
+# Graphical VAR with LASSO regularization is readily implemented in the graphicalVAR package
+
+# Time intervals are assumed equally spaced
+# Yet in typical EMA structure, there are no assessments at night time
+# Thus, specify beepvar and dayvar so the first beep of the day is not 
+# regressed on the last beep of the previous day
+p1 <- as.data.frame(p1)
+p1$id <- 1
+p1$beep <- rep(1:5, length.out=nrow(p1))
+p1$day <- rep(1:40, each=5)
+
+
+res <- graphicalVAR(p1, nLambda=20,
+                    vars=varnames,
+                    beepvar="beep",
+                    dayvar="day")
+
+res$PDC |> qgraph(layout="circle", theme="colorblind", labels=varnames, fade=F)
+res$PCC |> qgraph(layout="circle", theme="colorblind", labels=varnames, fade=F)
+
+# Directed network included a spurious autoregressive effect of PA,
+# but otherwise much improved recovery of true network structure
+
+# Centrality
+df <- centralityTable(res$PDC) |>
+  filter(measure %in% c("InStrength", "OutStrength"))
+
+df |>
+  ggplot(aes(x = node, y = value, group = measure)) +
+  geom_line(aes(linetype = measure), linewidth = 1) +
+  labs(x = '', y = 'z-score') +
+  scale_linetype_discrete(name = "Centrality") +
+  coord_flip() +
+  theme_bw()
+
+
+
+
--- SEPP_tut.R
+++ SEPP_tut.R
@@ -1,7 +1,7 @@
 
 #################################################
 ##                                             ##
-## Code for manuscript titled:                 ##
+## Supplementary code for manuscript titled:   ##
 ##                                             ##
 ## Network Approaches for Physical Activity    ##
 ## and Mental Health Research                  ##
@@ -18,10 +18,19 @@
 
 
 library(graphicalVAR)
-library(qgraph)
-library(corpcor)
 library(mlVAR)
+library(qgraph)
+library(dplyr)
+library(ggplot2)
 
+
+# Code are heavily adapted from the graphicalVAR package
+
+# Epskamp S (2021). _graphicalVAR: Graphical VAR for
+# Experience Sampling Data_. R package version 0.3,
+# <https://CRAN.R-project.org/package=graphicalVAR>.
+
+# https://github.com/SachaEpskamp/graphicalVAR
 
 
 # Graphical VAR model from single-subject multivariate time series -------------------------------------------------
@@ -39,18 +48,18 @@ varnames <- c("Depr", "Anhed", "Fatigue", "PA")
 
 # Sigma is the variance-covariance matrix after accounting for lag-1 temporal effects
 # and is used to estimate the contemporaneous network (conditional dependencies within time points)
-Sigma <- matrix(c(1.91,  0.69, 1.01,  0.87,
-                  0.69,  1.39, 0.14, -0.14,
-                  1.01,  0.14, 1.91,  1.22,
-                  0.87, -0.14, 1.22,  1.91),nrow=4,byrow=T)
+trueSigma <- matrix(c(1.91,  0.69, 1.01,  0.87,
+                      0.69,  1.39, 0.14, -0.14,
+                      1.01,  0.14, 1.91,  1.22,
+                      0.87, -0.14, 1.22,  1.91),nrow=4,byrow=T)
 
 # Get PCC (partial contemporaneous correlations) by standardizing 
 # the precision matrix (Kappa) and multiplying the off-diagonal elements by -1
-Kappa <- solve(Sigma)
-PCC <- -Kappa / (sqrt(diag(Kappa)) %*% t(sqrt(diag(Kappa))))
-diag(PCC) <- 1
-PCC
-qgraph(PCC, layout="circle", labels=varnames)
+trueKappa <- solve(trueSigma)
+truePCC <- -trueKappa / (sqrt(diag(trueKappa)) %*% t(sqrt(diag(trueKappa))))
+diag(truePCC) <- 1
+truePCC
+qgraph(truePCC, layout="circle", labels=varnames)
 
 # PA and fatigue are strongly associated within time points,
 # patient interview may indicate that strenuous activity results
@@ -69,127 +78,152 @@ qgraph(PCC, layout="circle", labels=varnames)
 # To aid in interpretability we obtain the partial directed correlations
 # by standardizing and transposing the beta matrix
 # (thus rows indicate the node of origin, and columns the destination )
-Beta <- matrix(c())
-
-
-
-
+trueBeta <- matrix(c(0.10, 0.60, -0.90, -0.81,
+                     0.30, 0.10,  0.00,  0.00,
+                     0.00, 0.00,  0.20, -0.75,
+                     0.00,-0.40, -0.70, -0.20), nrow=4,byrow=T)
 
 # PDC = partial directed correlations
 # conditional dependencies between time points
-PDC <- matrix(c(0.3,  0.02, -0.5,  0.3,
-                0.0,  0.3,   0.02, 0.3,
-                0.0, -0.5,   0.2,  0.5,
-                0.0,  0.0,  -0.3,  0.3), nrow=4,byrow=T)
+truePDC <- t(trueBeta / sqrt( diag(trueSigma) %o% diag(trueKappa) + trueBeta^2))
 
-qgraph(PDC, layout="circle", labels=varnames)
-
-
-# Simulate observations ---------------------------------------------------
-# decomposing the grpahicalVARsim() function
+qgraph(truePDC, layout="circle", labels=varnames)
 
 
 
 
+# Simulate N=1 observations from hypothetical true model ---------------------------------------------------
+
+set.seed(2024)
+p1 <- graphicalVARsim(nTime = 100,
+                      beta = trueBeta,
+                      kappa = trueKappa)
 
 
-# graphical VAR model: y[t] = Beta*y[t-1] + e[t] 
-# where e[t] ~ N(0, Sigma)
-
-# Sigma is the variance-covariance matrix after accounting for lag-1 temporal effects
-# and is used to estimate the contemporaneous network
-# from the partial correlation coefficients (conditional dependencies within time points)
-Sigma <- matrix(c(1.7, 0.4, -0.3, -1.3,
-                  0.4, 1.5, 0.5, -0.7,
-                  -0.3, 0.5, 1.3, 0.5,
-                  -1.3, -0.7, 0.5, 2.1), nrow=4,ncol=4, byrow=T)  
-
-# Check if positive definite
-is.positive.definite(Sigma)
-# Kappa is thus the precision matrix (inverse of Sigma)
-Kappa <- solve(Sigma)
-# Get PCC (partial contemporaneous correlations) by standardizing Kappa and multiplying by -1
-# PCC = - kappa[ij] / sqrt(kappa[ii] * kappa[jj])
-PCC <- -Kappa / (sqrt(diag(Kappa)) %*% t(sqrt(diag(Kappa))))
-diag(PCC) <- 1
-PCC
-qgraph(PCC, layout="circle", labels=varnames)
-
-# Beta encodes lagged regression effects
-# To aid in interpretability we obtain the partial directed correlations
-# by standardizing and transposing the beta matrix
-# (thus rows indicate the node of origin, and columns the destination )
-PDC <- t(beta / sqrt( diag(solve(kappa)) %o% diag(kappa) + beta^2))
+p1 <- as.data.frame(p1)
+colnames(p1) <- varnames
 
 
+# Fit the unregularized VAR model ------------------------------------------------------
+p1_unreg <- p1
 
-qgraph(PDC, layout="circle", labels=varnames)
-qgraph(pcc, layout="circle", labels=varnames)
+# Center and scale
+p1_unreg[, varnames] <- scale(p1_unreg[, varnames], center=T,scale=T)
 
-Beta <- matrix(c())
-
-
-
-PCC <- matrix(c(1.0, 0.8, -0.5, 0.8,
-                0.8, 1.0, 0.3, 0.05,
-                -0.5, 0.3, 1.0, -0.5,
-                0.8, 0.05, -0.5, 1.0), ncol=4,nrow=4,byrow=T)
-
-PCC <- -Kappa / (sqrt(diag(Kappa)) %*% t(sqrt(diag(Kappa))))
-diag(PCC) <- 1
-
-qgraph(PCC, layout="circle", labels=varnames)
+# Compute current and lagged data
+p1_current <- p1_unreg[-1,, drop=F] |> as.matrix()
+p1_lagged <- cbind(1, p1_unreg[-nrow(p1_unreg),,drop=F]) |> as.matrix()
 
 
-find_kappa <- function(PCC) {
-  sqrt_diag <- sqrt(diag(PCC))
-  return(-PCC * (sqrt_diag %*% t(sqrt_diag)))
-}
+SigmaHat <- cov(p1_unreg[, varnames])
+L1Hat <- cov(p1_lagged[, varnames], p1_current[, varnames])
+beta <- t(solve(SigmaHat) %*% L1Hat)
+kappa <- solve(SigmaHat - beta %*% SigmaHat %*% t(beta))
 
-# Find Kappa
-Kappa <- find_kappa(PCC)
+unreg_PCC <- -kappa / (sqrt(diag(kappa)) %*% t(sqrt(diag(kappa))))
+diag(unreg_PCC) <- 0
+unreg_PDC <- t(beta / sqrt( diag(solve(kappa)) %o% diag(kappa) + beta^2))
 
-PCC <- -Kappa / (sqrt(diag(Kappa)) %*% t(sqrt(diag(Kappa))))
-diag(PCC) <- 1
-
-qgraph(PCC, layout="circle", labels=varnames)
+qgraph(unreg_PCC, layout="circle", labels=varnames, fade=F)
+qgraph(unreg_PDC, layout="circle", labels=varnames, fade=F)
 
 
 
 
+# Many spurious edges. We want to obtain a sparse network where edges that are likely to be spurious are shrunk to 0
+# Thus we apply lasso regularization, with tuning parameters (lambda) controlling sparsity in Beta and Kappa
+# Tuning parameters are selected by minimizing the Extended Bayesian Information Criterion
+# The EBIC hyperparameter (gamma) is specified to prefer discovery (gamma=0)
+# or parsimony (gamma=0.5) 
+
+
+# Graphical VAR with LASSO regularization is readily implemented in the graphicalVAR package
+
+# Optional to have structure mimic EMA, collected 5x per day for 20 days
+# Time intervals are assumed equally spaced
+# Specify beepvar and dayvar so the first beep of the day is not 
+# regressed on the last beep of the previous day
+# p1$beep <- rep(1:5, 20)
+# p1$day <- rep(1:20, each=5)
+# str(p1)
+
+set.seed(2024)
+gvar1 <- graphicalVAR(p1, vars=varnames,
+                      nLambda=20, gamma=0.5, 
+                      scale=T,lags=1)
+
+gvar_pcc <- qgraph(gvar1$PCC, layout="circle", labels=varnames)
+gvar_pdc <- qgraph(gvar1$PDC, layout="circle", labels=varnames)
 
 
 
-
-PDC <- matrix(c(0.3,  0.02, -0.5,  0.3,
-                0.0,  0.3,   0.02, 0.3,
-                0.0, -0.5,   0.2,  0.5,
-                0.0,  0.0,  -0.3,  0.3), nrow=4,byrow=T)
+# The true edge between PA-Anhed in the contemporaneous model was not obtained
+# The small autoregressive effects were also not obtained
+# Otherwise, we can see better recovery of the true PCC and PDC models
 
 
 
-PDC <- t(beta / sqrt( diag(solve(kappa)) %o% diag(kappa) + beta^2))
+# Centrality --------------------------------------------------------------
 
 
-# To compute the partial correlation coefficients
+# In-strength and out-strength: 
+# sum of magnitudes of edges entering and exiting a node 
 
-is.positive.definite()
+df <- centralityTable(gvar_pdc) %>%
+  filter(measure %in% c("InStrength", "OutStrength"))
 
-eigen(randomsim$kappa)
-is.positive.definite(randomsim$kappa)
-eigen(randomsim$beta)
-is.positive.definite(randomsim$beta)
-
-
-ntime <- 50
-
-
-
-
-
+df %>%
+  ggplot(aes(x = node, y = value, group = measure)) +
+  geom_line(aes(linetype = measure), linewidth = 1) +
+  labs(x = '', y = 'z-score') +
+  scale_linetype_discrete(name = "Centrality") +
+  coord_flip() +
+  theme_bw()
 
 
 
+# Multilevel VAR -----------------------------------------------------------
+
+# Simulate a random model and observations
+set.seed(1)
+ml_sim <- mlVARsim(nPerson=50,
+                   nNode=4,
+                   nTime=100,
+                   lag=1)
+
+names(ml_sim$Data) <- c(varnames, "ID")
+# Mimic EMA data 5x/day for 20 days
+ml_sim$Data$beep <- rep(1:5)
+ml_sim$Data$day <- rep(1:20, each=5)
+
+
+str(ml_sim$Data)
+
+
+# Fit the model
+mlvar_mod <- mlVAR(ml_sim$Data,
+                   vars=varnames,
+                   lags=1,
+                   idvar="ID",
+                   dayvar = "day",
+                   beepvar = "beep",
+                   temporal="correlated" #correlated random effects
+                   )
+
+print(mlvar_mod)
+
+# Parameter estimates
+summary(mlvar_mod) 
+
+# Compare simulated model with fitted model
+plot(ml_sim, "temporal", title = "True temporal relationships", layout = "circle")
+plot(mlvar_mod, "temporal", title = "Estimated temporal relationships", layout = "circle", labels=varnames)
+
+
+plot(ml_sim, "contemporaneous", title = "True contemporaneous relationships",
+     layout = "circle")
+plot(mlvar_mod, "contemporaneous", title = "Estimated contemporaneous relationships",
+     layout = "circle")
 
 
 
